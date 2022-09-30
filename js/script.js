07fbb45c313947294a1addaad1b75e3c918d3139
adhistir <adhistiroshandy808@gmail.com>
2022-09-30 16:48:32 +0700

    Tugas6

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiperPopuler", {
+    grabCursor: true,
+    spaceBetween: 20,
+    centeredSlides: false,
+    slidesPerView: "auto",
+    navigation: {
+        nextEl: ".bxs-chevron-right-circle",
+        prevEl: ".bxs-chevron-left-circle",
+      },
+  });
